cfcbf9741e5249543004672c00c0a111bdb3f7da
Meaghan Jones and Stephanie Gurung <stephanie@>
2016-09-07 11:38:06 -0700

    save and display a new link inputted by the user

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,4 +2,24 @@ class LinksController < ApplicationController
   def index
     @links = Link.all
   end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      flash[:notice] = "Link successfully added!"
+      redirect_to links_path
+    else
+      flash[:alert] = "Link not added"
+      render :new
+    end
+  end
+
+private
+  def link_params
+    params.require(:link).permit(:title, :url, :source, :username)
+  end
 end
